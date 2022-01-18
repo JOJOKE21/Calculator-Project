95d2cfa4dd4a84819ff2cfd7a55ddea1483a115a
JOJOKE21 <kingjordan2122@gmail.com>
2022-01-18 12:26:54 -0500

    L

--- Script.js
+++ Script.js
@@ -3,17 +3,33 @@ function clearScreen() {
     document.getElementById("display").value = "";
    }
    
-   // This function display values
-   function display(value) {
-    document.getElementById("display").value += value;
- 
-   }
+//    function backSpace(value){
+//     document.getElementsById("delete").value = value.slice(0,-1);
+// }
 
    
-   
+
+   // This function display values
+   function display(value) {
+
+       
+    document.getElementById("display").value += value;
+ 
+    // document.getElementsById("display")
+    //   .value = (value/100)*value
+
+   }
+  
+
+  
    // This function evaluates the expression and return result
    function calculate() {
     const p = document.getElementById("display").value;
     const q = eval(p);
     document.getElementById("display").value = q;
    }
+
+
+   
+
+   
